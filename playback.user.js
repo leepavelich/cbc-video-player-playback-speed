02deb56f17e3311d5ffcd097a72619ea5b38d036
Lee Pavelich <leepavelich@users.noreply.github.com>
2024-08-02 17:35:59 -0600

    Update playback.user.js

--- playback.user.js
+++ playback.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CBC Olympics Video Speed Modifier
 // @namespace    leepavelich
-// @version      0.3
+// @version      0.4
 // @description  Adds playback speed controls to CBC Olympics video player
 // @match        https://www.cbc.ca/*
 // @grant        none
@@ -11,17 +11,21 @@
     'use strict';
 
     const speeds = [0.5, 1.0, 1.5, 2.0];
-    let currentSpeedIndex = speeds.indexOf(1.0); // Default to normal speed
-
+    let currentSpeedIndex = speeds.indexOf(1.0);
     const createSpeedControl = () => {
-        const speedControl = document.createElement('div');
+        const speedControl = document.createElement('button');
         speedControl.className = 'speed-control';
-        speedControl.style.cssText = 'display: inline-block; margin-right: 10px; position: relative;';
-        speedControl.innerHTML = `
-          <button class="speed-btn" style="background: none; border: none; color: white; font-size: 14px; cursor: pointer; padding: 5px;">
-            ${speeds[currentSpeedIndex]}x
-          </button>
+        speedControl.style.cssText = `
+            background: none;
+            border: none;
+            color: white;
+            font-size: 14px;
+            cursor: pointer;
+            padding: 8px 5px;
+            margin-right: 10px;
+            vertical-align: middle;
         `;
+        speedControl.textContent = `${speeds[currentSpeedIndex]}x`;
         return speedControl;
     };
 
@@ -33,12 +37,11 @@
     };
 
     const addSpeedControl = () => {
-        const controlRack = document.querySelector('.phx-controls-right');
-        if (controlRack && !document.querySelector('.speed-control')) {
+        const fullscreenBtn = document.querySelector('fullscreen-btn');
+        if (fullscreenBtn && !document.querySelector('.speed-control')) {
             const speedControl = createSpeedControl();
-            const speedBtn = speedControl.querySelector('.speed-btn');
-            speedBtn.addEventListener('click', () => updateSpeed(speedBtn));
-            controlRack.insertBefore(speedControl, controlRack.firstChild);
+            speedControl.addEventListener('click', () => updateSpeed(speedControl));
+            fullscreenBtn.parentNode.insertBefore(speedControl, fullscreenBtn);
         }
     };
 
